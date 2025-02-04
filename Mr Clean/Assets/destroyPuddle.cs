ab1ccb22533fa1c81b3b9dfcd135bfaac00abaa0
MarcosHBlanco <124410355+MarcosHBlanco@users.noreply.github.com>
2025-02-03 20:32:00 -0800

    Changes to rules on DestroyPuddle Script
    Not puddle shrinks instead of being destroyed depending on its scale.

--- Mr Clean/Assets/destroyPuddle.cs
+++ Mr Clean/Assets/destroyPuddle.cs
@@ -1,11 +1,17 @@
+using System;
 using UnityEngine;
 
 public class destroyPuddle : MonoBehaviour
 {   
     public float requiredDuration = 3f;
     public float timer = 0f;
+    public float minScale = 3f;
     bool isOnPuddle = false;
+    private Vector3 initialScale;
 
+    private void Start(){
+        initialScale = transform.localScale;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player")){
@@ -27,8 +33,20 @@ public class destroyPuddle : MonoBehaviour
         if(isOnPuddle){
             timer += Time.deltaTime;
             if(timer >= requiredDuration){
-                Destroy(gameObject);
+                float currentScaleX = transform.localScale.x;
+                if(currentScaleX > 3){
+                    ShrinkPuddle(3f);
+                } else {
+                    Destroy(gameObject);
+                }
+                timer = 0f;
             }
         }
+    }
+    public void ShrinkPuddle(float amount){
+        // float newScaleX = Mathf.Max(transform.localScale.x - amount, minScale);
+        // float newScaleZ = Mathf.Max(transform.localScale.z - amount, minScale);
+
+        transform.localScale = new Vector3(transform.localScale.x - amount, transform.localScale.y, transform.localScale.z - amount);
     }
 }
